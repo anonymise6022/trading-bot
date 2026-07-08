d6ec5bd8b0ba0c5c477842b2e62bbdc4b73ab401
anonymise6022 <laukelvin123123@gmail.com>
2026-07-08 16:32:40 -0400

    strategy stuffs

--- lib/trading_strategy.py
+++ lib/trading_strategy.py
@@ -1,4 +1,16 @@
+from training import model
 import numpy as np
 import pandas as pd
 
-y_hat = 
+df = pd.read_csv('historical_data.csv')
+
+weights = model.weight.data
+print(f"weights: {weights}")
+biases = model.bias.data
+print(f"biases: {biases}")
+
+lags = df.iloc[:, -6:].values
+
+for lag, weights in zip(lags, weights):
+    pred += lag * weights
+    print(f"Prediction for{pred}")
