Fixed data leakage and use MA

lib/btc_data.py
@@ -2,21 +2,41 @@ import pandas as pd
 import numpy as np
 
 symbol = 'BTC/USDT'
-url = 'BTCUSDT-1h.csv'  # Replace with the actual URL of your CSV file
-df = pd.read_csv(url) #reading the data from the url
+url = 'BTCUSDT-1h.csv'
 
-# Calculate log return
+df = pd.read_csv(url)
+
+# Calculate log return (target)
 df['close_log_return'] = np.log(df['close'] / df['close'].shift(1))
 
-# Lag it from 1 to 6 candles and add it to csv file
+# Previous returns as features
 for lag in range(1, 7):
-    df[f'close_log_return_lag_{lag}'] = df['close_log_return'].shift(lag)
+    df[f'close_log_return_lag_{lag}'] = (
+        df['close_log_return'].shift(lag)
+    )
 
-# Remove rows with NaN values created by shift()
+# Calculate moving averages
+df["sma_20"] = df["close"].rolling(20).mean()
+df["sma_50"] = df["close"].rolling(50).mean()
+
+# Previous candle values only
+df["sma_20_prev"] = df["sma_20"].shift(1)
+df["sma_50_prev"] = df["sma_50"].shift(1)
+
+# Detect crossover
+df["sma_cross"] = 0
+
+# bullish crossover
+df.loc[(df["sma_20_prev"] <= df["sma_50_prev"]) & (df["sma_20"] > df["sma_50"]),"sma_cross"] = 1
+
+# bearish crossover
+df.loc[(df["sma_20_prev"] >= df["sma_50_prev"]) & (df["sma_20"] < df["sma_50"]),"sma_cross"] = -1
+
+df["sma_trend_strength"] = (df["sma_20"].shift(1) -df["sma_50"].shift(1)) / df["sma_50"].shift(1)
+
+# Remove missing rows
 df = df.dropna()
 
-#save it and overwrite the csv file with the new columns
-df.to_csv('BTCUSDT-1h.csv', index=False)
-print(f"Historical data for {symbol} saved to 'BTCUSDT-1h.csv'.")
+df.to_csv('BTCUSDT-1hNEW.csv', index=False)
 
-
+print(f"Historical data for {symbol} saved to BTCUSDT-1h.csv")

lib/latest_data.py
@@ -27,7 +27,7 @@ def download_his_data():
     df['close_log_return'] = np.log(df['close'] / df['close'].shift(1))
 
     # Lag it from 1 to 6 candles
-    for lag in range(1, 7):
+    for lag in range(1, 6):
         df[f'close_log_return_lag_{lag}'] = df['close_log_return'].shift(lag)
 
     # Remove rows with NaN values created by shift()
@@ -83,7 +83,7 @@ def download_his_data():
     merged["oi_momentum"] = (merged["notional_oi"] / merged["notional_oi"].shift(1)) - 1
 
     # creating lags
-    for lag in range(1, 7):
+    for lag in range(1, 6):
         merged[f"oi_momentum_lag_{lag}"] = (
             merged["oi_momentum"].shift(lag)
         )   

lib/training.py
@@ -5,6 +5,7 @@ import numpy as np
 import random
 import os
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 # ask which data to use: old static one or new latest 1000 candles
 user_input = input("Which dataset do you want to use? 40k or latest?: ")
@@ -15,7 +16,7 @@ if user_input == "latest":
 
 # if user wants to use the old static dataset
 else:
-     btcusdt = pd.read_csv('BTCUSDT-1h.csv', parse_dates=["open_time"], index_col='open_time')
+     btcusdt = pd.read_csv('BTCUSDT-1hNEW.csv', parse_dates=["open_time"], index_col='open_time')
 
 df = pd.DataFrame()
 
@@ -45,7 +46,7 @@ torch.cuda.manual_seed_all(SEED)
 
 target = 'close_log_return'
 
-use_oi = True
+use_oi = False
 feature_log_cols = [
     'close_log_return_lag_1',
     'close_log_return_lag_2',
@@ -53,6 +54,8 @@ feature_log_cols = [
     'close_log_return_lag_4',
     'close_log_return_lag_5',
     'close_log_return_lag_6',
+    'sma_cross',
+    'sma_trend_strength',
 ]
 
 feature_oi_cols = [
@@ -61,7 +64,6 @@ feature_oi_cols = [
     'oi_momentum_lag_3',
     'oi_momentum_lag_4',
     'oi_momentum_lag_5',
-    'oi_momentum_lag_6',
 ]
 
 #check if user wants to use open interest features
@@ -80,8 +82,17 @@ print(f"\n========== Training model  ==========")
 # 1. CREATE TENSORS FROM DATAFRAME
 # -------------------------------------------------------
 
-X_train = torch.tensor(btcusdt_train[feature_cols].values,dtype=torch.float32)
-X_test = torch.tensor(btcusdt_test[feature_cols].values,dtype=torch.float32)
+scaler = StandardScaler()
+
+# fit ONLY on training data
+X_train_scaled = scaler.fit_transform(btcusdt_train[feature_cols])
+
+# use the same scaler on test data
+X_test_scaled = scaler.transform(btcusdt_test[feature_cols])
+
+# convert to tensors
+X_train = torch.tensor(X_train_scaled, dtype=torch.float32)
+X_test = torch.tensor(X_test_scaled, dtype=torch.float32)
 
 model_size = len(feature_cols)
 model = nn.Linear(model_size, 1)
@@ -162,3 +173,5 @@ direction_correct = (
 ).mean()
 
 print("Directional Accuracy:", direction_correct)
+
+print("Baseline accuracy:", (actual_np > 0).mean())